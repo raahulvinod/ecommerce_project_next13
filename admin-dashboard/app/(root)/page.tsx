0755ce599bca47b9be5e1fbfbc7cc56f012406e9
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-01 13:26:13 +0530

    first commit

---
+++ admin-dashboard/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello Admin</p>
+      <div className="p-4">
+        <Button>Click Me</Button>
+      </div>
+    </div>
+  );
+}
