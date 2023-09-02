71a976da7d15de6a52c2386f12a5cd9ae08fbadb
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-02 15:18:39 +0530

    Use store modal custom hook added

--- admin-dashboard/app/(root)/page.tsx
+++ admin-dashboard/app/(root)/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+
+import { Modal } from '@/components/ui/modal';
 import { UserButton } from '@clerk/nextjs';
 
 const SetupPage = () => {
   return (
     <div className="p-4">
       <UserButton afterSignOutUrl="/" />
+      <Modal title="test" descripton="test desc" isOpen onClose={() => {}}>
+        children
+      </Modal>
     </div>
   );
 };
