bf8c306c0c4e1fc3a2074eb07dcc6f831eb00805
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-02 15:50:07 +0530

    Custom Reusable Modal Added

--- admin-dashboard/app/(root)/page.tsx
+++ admin-dashboard/app/(root)/page.tsx
@@ -1,17 +1,20 @@
 'use client';
 
-import { Modal } from '@/components/ui/modal';
-import { UserButton } from '@clerk/nextjs';
+import { useEffect } from 'react';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
-      <Modal title="test" descripton="test desc" isOpen onClose={() => {}}>
-        children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 };
 
 export default SetupPage;
